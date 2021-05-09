dad23c65d9e53041de301e70f9f4979931d62af8
ybsid <ybsidstar@gmail.com>
2021-05-09 21:22:49 +0530

    create spring-boot main class

---
+++ src/main/java/com/ybsid/kafka/kstreamconsumer/KstreamConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.ybsid.kafka.kstreamconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KstreamConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KstreamConsumerApplication.class, args);
+	}
+
+}
